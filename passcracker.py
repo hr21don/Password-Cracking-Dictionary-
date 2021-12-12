d1c2bb7250184334b2c60887fe5cee6b9462a0a5
Helitha Rupasinghe <91548582+hr21don@users.noreply.github.com>
2021-12-12 12:40:56 +0000

    Update passcracker.py

--- passcracker.py
+++ passcracker.py
@@ -13,8 +13,6 @@ Geeksforgeeks(2020).https://www.geeksforgeeks.org/md5-hash-python/.Date Accessed
 ToolZilla Starting Files
 
 """
-flag = 0
-
 title =  "WELCOME To TOOLZILLA PASS CRACKER.... WHY NOT STAY A WHILE?\n"
 for char in title:
     sleep(0.05)
